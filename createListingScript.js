794aeda4766af78f55cdb84c0553a261c4285277
Jos Kotze <jpkotze148@gmail.com>
2023-05-28 19:42:19 +0200

    google book cover API

--- createListingScript.js
+++ createListingScript.js
@@ -12,7 +12,28 @@ function showListing(str) {
     xhttp.send();
   }
 
+function getBookCover() {
+  var isbn = $('.book').data('isbn');
 
+  $.ajax({
+    dataType: 'json',
+    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+    success: handleResponse
+  });
+
+  function handleResponse( response ) {
+    $.each( response.items, function( i, item ) {
+      
+      var title    = item.volumeInfo.title,
+          author   = item.volumeInfo.authors[0],        
+          thumb    = item.volumeInfo.imageLinks.thumbnail;
+      
+      $('.title').text( title );
+      $('.author').text( author );
+      $('.thumbnail').attr('src', thumb);
+    });
+  }
+}
   
 function CreateListingObj() {
 
@@ -33,7 +54,6 @@ function CreateListingObj() {
 
     
 }
-
 
 const createListingCard = () => {
     listings.forEach((listing) => {
